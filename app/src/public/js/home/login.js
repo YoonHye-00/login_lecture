bd2fc6a7a0c33f6ceba14bc9a39aed94d521c3cf
YoonHye-00 <Email address>
2021-11-22 00:04:43 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("hello");
+//DOM -> html에 존재하는 데이터를 가져와서 javascript에서 제어 가능
+
+const id = document.querySelector("#id"),   //id가 id인 태그를 가져옴
+    password = document.querySelector("#password"), //id가 password인 태그를 가져옴
+    loginButton = document.querySelector("button"); //button 태그를 가져옴
+
+loginButton.addEventListener("click", login);   //로그인 버튼이 눌리면 login 함수 실행
+
+function login(){
+    const req = {
+        id : id.value,
+        password : password.value
+    }
+    console.log(req);
+}
+
+
