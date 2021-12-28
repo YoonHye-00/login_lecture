7fc3b2eac28791c77be07891cb6d87f1a0b838fa
YoonHye-00 <Email address>
2021-12-29 06:15:52 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 //서버 <-> 클라이언트 전달 경로
 router.get("/", ctrl.output.home);
 router.get('/login',ctrl.output.login);
+router.get('/register',ctrl.output.register);
 router.post('/login',ctrl.process.login);    //front-end가 전달한 로그인 데이터를 받아서 로그인 기능을 처리해주는 API. 
                                              //login.js에서 fetch를 통해 서버로 req 전달을 수행하기 위해 필요함
 
