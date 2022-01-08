3a99c93899a3ca7a6d7af37bfc3f9637a9f9c649
YoonHye-00 <Email address>
2022-01-08 11:50:39 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,24 @@
 "use strict";
 
+const fs = require("fs").promises;   //파일 시스템. users 테이블(users.json)에 접근해서 데이터를 읽어오기 위함.(.promisis -> promise를 반환함)
+
 class UserStorage {     //class 자체에서 users에 접근하기 위해 static을 붙여줌 (UserStorage.users 가능 (public인 경우)). 아니면 const userStorage = new UserStorage(); 한 뒤에 가능.
-    static #users = {   //# -> private 변수로 선언. 외부에서 접근 할 수 X. 데이터 은닉화
-        id: ["Aries", "bbbb", "cccc"],
-        password: ["dkfltm", "2222", "3333"],
-        name : ["박윤혜", "나", "다"]
+    
+    static #getUserInfo(id, data) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);   // => [id, password, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
     }
 
     static getUsers(...fields) { //외부에서 접근할 수 있도록 메소드 생성. users에서 원하는 키-값만 가져올 수 있도록 파라미터를 ...fileds로 함
                                  //파라미터로 넘긴 데이터들이 fields에 배열 형태로 저장됨. 만약 id, password를 넘겼으면 ['id', 'password'] 이렇게 되는거임
-        const users = this.#users;  
+        //const users = this.#users;  
         
         //fields에 대한 원소가 하나씩 순회됨(reduce 반복문). newUsers -> fields 배열의 초기값(내가 지정 가능)
         //field -> 다음 변수들이 들어옴. field 변수에 fields 배열의 값들이 하나씩 들어감.
@@ -23,19 +32,17 @@ class UserStorage {     //class 자체에서 users에 접근하기 위해 static
     }
 
     static getUserInfo(id) {    //id에 해당하는 id, password, name을 object형태로 return해주는 함수
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);   // => [id, password, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+        
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(id, data)
+            })
+            .catch(console.error);
     }
 
     static save(userInfo) {     //이 코드는 서버가 재가동 되기 전까지는 문제가 없지만 서버가 재가동되면 #users가 다시 원래대로 초기화된다는 문제점이 있음.
-        const users = this.#users;
+        //const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -19,9 +19,9 @@ const output = {
 }
 
 const process = {
-    login : (req,res) => {
+    login : async (req,res) => {
         const user = new User(req.body);
-        const response = user.login();
+        const response = await user.login();
         return res.json(response); //response를 클라이언트에게 json형태로 응답해줌.
      },
 
